34f94f84d8049024deab138a92cfc65c40635fe0
Sebastian Göb <sebastiancgoeb@gmail.com>
2017-11-24 08:23:05 +0100

    Improved keybindings

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -37,12 +37,21 @@ setopt incappendhistory
 # enable implicit `cd`
 setopt autocd
 
-# keybindings
+# generic keybindings
 bindkey -e
-bindkey "^[[A" history-substring-search-up
-bindkey "^[[B" history-substring-search-down
-bindkey "^[OH" beginning-of-line
-bindkey "^[OF" end-of-line
-bindkey "^[[2~" overwrite-mode
-bindkey "^[^[[D" backward-word
-bindkey "^[^[[C" forward-word
+bindkey "^[[A" history-substring-search-up # up
+bindkey "^[[B" history-substring-search-down # down
+bindkey "^[[2~" overwrite-mode # ins
+
+# macOS keybindings
+bindkey "^[^[[D" backward-word # alt + left
+bindkey "^[^[[C" forward-word # alt + right
+bindkey "^[OH" beginning-of-line # cmd + left ?
+bindkey "^[OF" end-of-line # cmd + right ?
+
+# Windows
+bindkey "^[[1;5D" backward-word # ctrl + left
+bindkey "^[[1;5C" forward-word # ctrl + right
+bindkey "^[[H" beginning-of-line # home
+bindkey "^[[F" end-of-line # end
+bindkey "^[[3~" delete-char # del
