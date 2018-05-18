ce85549094493b66c26c7314e7b919b5c0fd37b5
Sebastian Göb <sebastiancgoeb@gmail.com>
2018-05-18 11:16:31 +0200

    Improve error handling in zplug setup

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -8,34 +8,37 @@ case "$OSTYPE" in
 esac
 
 if [[ -v ZPLUG_HOME ]]; then
-    # initialize zplug
-    source $ZPLUG_HOME/init.zsh
+    if [[ -f $ZPLUG_HOME/init.zsh ]]; then
+        # initialize zplug
+        source $ZPLUG_HOME/init.zsh
 
-    if ( command -v zplug &> /dev/null ); then
+        if ( command -v zplug &> /dev/null ); then
+            # credit: https://terriblecode.com/blog/zplug-from-a-former-oh-my-zsh-user/
+            # async for zsh, used by pure
+            zplug "mafredri/zsh-async", from:github, defer:0
+            # pure theme
+            zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+            # syntax highlighting for commands, load last (defer)
+            zplug "zsh-users/zsh-syntax-highlighting", from:github, defer:3
+            # partial match history entries
+            zplug "zsh-users/zsh-history-substring-search"
 
-        # credit: https://terriblecode.com/blog/zplug-from-a-former-oh-my-zsh-user/
-        # async for zsh, used by pure
-        zplug "mafredri/zsh-async", from:github, defer:0
-        # pure theme
-        zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
-        # syntax highlighting for commands, load last (defer)
-        zplug "zsh-users/zsh-syntax-highlighting", from:github, defer:3
-        # partial match history entries
-        zplug "zsh-users/zsh-history-substring-search"
-
-        # install plugins if there are plugins that have not been installed
-        if ! zplug check --verbose; then
-            printf "Install missing zplug plugins? [y/N]: "
-            if read -q; then
-                echo; zplug install
+            # install plugins if there are plugins that have not been installed
+            if ! zplug check --verbose; then
+                printf "Install missing zplug plugins? [y/N]: "
+                if read -q; then
+                    echo; zplug install
+                fi
             fi
-        fi
 
-        # finally, source plugins and add commands to $PATH
-        zplug load
+            # finally, source plugins and add commands to $PATH
+            zplug load
+        else
+            echo "Unable to initialize zplug. Found $ZPLUG_HOME/init.zsh but zplug command was not registered" >&2
+        fi
     else
-        echo "zplug not installed"
+        echo "Unable to initialize zplug. Can't find $ZPLUG_HOME/init.zsh" >&2
     fi
 else
-    echo "zplug home not defined"
+    echo "Unable to initialize zplug. \$ZPLUG_HOME not defined" >&2
 fi
