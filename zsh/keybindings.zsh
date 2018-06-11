403ddbb0d933866a8194fa6282cbcf081c939e82
Sebastian Göb <sebastiancgoeb@gmail.com>
2018-06-11 08:45:16 +0200

    Fixed some keybindings on Linux and IntelliJ

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,10 +1,10 @@
-# generic keybindings
+# generic
 bindkey -e
 bindkey "^[[A" history-substring-search-up # up
 bindkey "^[[B" history-substring-search-down # down
 bindkey "^[[2~" overwrite-mode # ins
 
-# macOS keybindings
+# macOS
 bindkey "^[^[[D" backward-word # alt + left
 bindkey "^[^[[C" forward-word # alt + right
 bindkey "^[OH" beginning-of-line # cmd + left ?
@@ -16,3 +16,11 @@ bindkey "^[[1;5C" forward-word # ctrl + right
 bindkey "^[[H" beginning-of-line # home
 bindkey "^[[F" end-of-line # end
 bindkey "^[[3~" delete-char # del
+
+# Linux
+bindkey "^[OA" history-substring-search-up # up
+bindkey "^[OB" history-substring-search-down # down
+
+# IntelliJ on Linux
+bindkey "^[O5D" backward-word # ctrl-left
+bindkey "^[O5C" forward-word # ctrl-right
