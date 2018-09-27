007cc51c3f1118e143ea8dc3912a362b080a9179
Sebastian Göb <sebastian.goeb@detim.de>
2018-09-27 14:24:19 +0200

    stopwatch

--- posix.d/030-misc.sh
+++ posix.d/030-misc.sh
@@ -11,3 +11,8 @@ export LC_ALL="en_GB.UTF-8"
 # User Binaries
 export PATH="$PATH:$HOME/.bin"
 
+stopwatch() {
+    date1=`date +%s`; while true; do 
+        echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
+    done
+}
