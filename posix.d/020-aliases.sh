b041a6d2aba963bdb80aa35a16eab23a7190dacb
Sebastian Göb <sebastian.goeb@detim.de>
2018-09-27 14:22:53 +0200

    More git aliases in own script

--- posix.d/020-aliases.sh
+++ posix.d/020-aliases.sh
@@ -11,21 +11,6 @@ alias la="ls -A"
 alias l="ls -lh"
 alias ll="ls -lah"
 
-# git
-alias gst="git status"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias ga="git add"
-alias gaa="git add --all"
-alias gc="git commit"
-alias gco="git checkout"
-alias gcm="git checkout master"
-alias gl="git log --graph --all --oneline --decorate"
-alias gf="git fetch"
-alias gpl="git pull"
-alias gpu="git push"
-alias gm="git merge"
-alias gb="git branch"
 
 # kubernetes
 alias kc="kubectl"
