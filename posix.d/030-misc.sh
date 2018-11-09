a5a44c9c11b599e40d186de840a900a1ae914102
Sebastian Göb <sebastian.goeb@detim.de>
2018-11-09 11:18:31 +0100

    formatting

--- posix.d/030-misc.sh
+++ posix.d/030-misc.sh
@@ -12,7 +12,7 @@ export LC_ALL="en_GB.UTF-8"
 export PATH="$PATH:$HOME/.bin"
 
 stopwatch() {
-    date1=`date +%s`; while true; do 
+    date1=`date +%s`; while true; do
         echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
     done
 }
