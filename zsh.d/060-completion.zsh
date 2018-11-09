89c5a3822dd19e98f5649e160a0a0cf3be682860
Sebastian Göb <sebastian.goeb@detim.de>
2018-11-09 11:17:30 +0100

    helm completion

--- zsh.d/060-completion.zsh
+++ zsh.d/060-completion.zsh
@@ -38,3 +38,7 @@ zstyle ':completion:*' use-compctl false
 if [ $commands[kubectl] ]; then
     source <(kubectl completion zsh)
 fi
+
+if [ $commands[helm] ]; then
+    source <(helm completion zsh)
+fi
