35f3ffa7725841ab1512a656cee6e691cf67fc1c
Sebastian Göb <sebastiancgoeb@gmail.com>
2019-02-20 09:31:52 +0100

    cleanup os detection

--- wallpapers.sh
+++ wallpapers.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-PICTURES_FOLDER="$HOME/.wallpapers"
+# $OSTYPE or uname
+# https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
+# https://github.com/dylanaraps/neofetch/issues/433
 
+PICTURES_FOLDER="$HOME/.wallpapers"
 case "$OSTYPE" in
-    bsd*)
-    echo "detected bsd, doing nothing"
-    ;;
     darwin*)
-    echo "detected macOS, installing wallpapers"
-    osascript wallpapers.applescript "$HOME/.wallpapers" 1 true 86400
-    ;;
+      echo "detected macOS, configuring wallpapers"
+      osascript wallpapers.applescript ${PICTURES_FOLDER} 1 true 86400
+      ;;
+    linux*)
+      echo "detected linux, configuring gnome wallpapers"
+      ./wallpapers_gnome.py -f ${PICTURES_FOLDER} --interval 1d --random
+      ;;
     *)
-    echo "detected something else, configuring gnome wallpaper slideshow"
-    ./wallpapers_gnome.py -f "$HOME/.wallpapers" --interval 1d --random
-    ;;
+      echo "detected something else, doing nothing"
+      ;;
 esac
