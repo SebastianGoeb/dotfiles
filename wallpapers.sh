2ed21a8a59d041856a07d005c8f874bb7018d1bf
Sebastian Göb <sebastiancgoeb@gmail.com>
2020-01-22 20:45:23 +0100

    move wallpaper copy into wallpapers.sh script

--- wallpapers.sh
+++ wallpapers.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+PICTURES_FOLDER="$HOME/.wallpapers"
+
+echo "overwriting ${PICTURES_FOLDER}"
+rm -rf ${PICTURES_FOLDER}
+cp -R wallpapers ${PICTURES_FOLDER}
+
 # $OSTYPE or uname
 # https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 # https://github.com/dylanaraps/neofetch/issues/433
 
-PICTURES_FOLDER="$HOME/.wallpapers"
 case "$OSTYPE" in
     darwin*)
       echo "detected macOS, configuring wallpapers"
-      osascript wallpapers.applescript ${PICTURES_FOLDER} 1 true 86400
+      osascript wallpapers.applescript "${PICTURES_FOLDER}" 1 true 86400
       ;;
     linux*)
       echo "detected linux, configuring gnome wallpapers"
-      ./wallpapers_gnome.py -f ${PICTURES_FOLDER} --interval 1d --random
+      ./wallpapers_gnome.py -f "${PICTURES_FOLDER}" --interval 1d --random
       ;;
     *)
       echo "detected something else, doing nothing"
