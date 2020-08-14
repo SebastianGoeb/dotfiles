80a138a21b849aff830d61826c141caf948e7431
Sebastian Göb <sebastian.goeb@detim.de>
2020-08-14 16:30:36 +0200

    don't prefer helm2 anymore

--- posix.d/030-misc.sh
+++ posix.d/030-misc.sh
@@ -11,11 +11,6 @@ export LC_ALL="en_GB.UTF-8"
 # User Binaries
 export PATH="$PATH:$HOME/.bin"
 
-# helm 2 path in macos
-case "$OSTYPE" in
-    darwin*)    export PATH="/usr/local/opt/helm@2/bin:$PATH" ;;
-esac
-
 stopwatch() {
     date1=`date +%s`; while true; do
         echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
