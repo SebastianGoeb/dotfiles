0db730df691c5bfb2c3fc2588243d4b5120ecc93
Sebastian Göb <sebastian.goeb@detim.de>
2021-01-25 19:50:00 +0100

    improve sherlock kafka aliases

--- posix/040-rikern.sh
+++ posix/040-rikern.sh
@@ -1,70 +1,76 @@
-function _export_namespace() {
-  if [[ "$1" == "rdl" ]]; then
-    export NAMESPACE="ridi-dev-live"
-  elif [[ "$1" == "rtl" ]]; then
-    export NAMESPACE="ridi-test-live"
-  elif [[ "$1" == "rpfpl" ]]; then
-    export NAMESPACE="ridi-prd-fpl"
-  else
-    export NAMESPACE="$1"
+# Kafka Tools Aliases
+function _run_in_sherlock_container() {
+  local confluent_version=${CONFLUENT_VERSION:-"5.3.3"}
+
+  # use first positional argument as kafka command, and rest as arguments
+  local kafka_command=$1
+  shift
+
+  local namespace=$(kubectl config view --minify | yq -j r - | jq -r '.contexts[0].context.namespace // "default"')
+  local ingress_infix=$(kubectl config view --minify | yq -j r - | jq -r '.contexts[0].context.cluster' |
+    sed -E 's/(.+)-(.+)-(.+)/\3-\2-\1/g' |
+    sed 's/ridi/ridienste/g' |
+    sed 's/riplf/riplattform/g')
+
+  # default image ist kafka
+  local confluent_image="confluentinc.bwp/cp-kafka"
+
+  local extra_args=()
+
+  # if working with avro, use schema registry image and properties
+  if [[ $kafka_command =~ .*avro.* ]]; then
+    confluent_image="confluentinc.bwp/cp-schema-registry"
+    extra_args+=(--property "schema.registry.url=https://schemaregistry.${namespace}.rikern-ingress-${ingress_infix}.reisendeninfo.aws.db.de:8081")
   fi
+
+  # use correct config option based on type of kafka command
+  if [[ $kafka_command =~ .*consumer.* ]]; then
+    extra_args+=(--consumer.config "/config.properties")
+  elif [[ $kafka_command =~ .*producer.* ]]; then
+    extra_args+=(--producer.config "/config.properties")
+  else
+    extra_args+=(--command-config "/config.properties")
+  fi
+
+  docker run --rm -it "dockerregistry.reisendeninfo.aws.db.de:5000/${confluent_image}:${CONFLUENT_VERSION}" \
+    "${kafka_command}" \
+    --bootstrap-server "kafka.${namespace}.rikern-ingress-${ingress_infix}.reisendeninfo.aws.db.de:9092" \
+    "${extra_args[@]}" \
+    "${@}"
 }
 
-function kpfargo() {
-  _export_namespace "$1"
-  export PORT=${2:-2746}
-  kubectl port-forward -n "$NAMESPACE" svc/argo-"$NAMESPACE"-server $PORT:2746
-}
+cp_kafka_tools=(
+  # from cp-kafka
+  kafka-acls
+  kafka-consumer-groups
+  kafka-log-dirs
+  kafka-replica-verification
+  kafka-topics
+  kafka-broker-api-versions
+  kafka-consumer-perf-test
+  kafka-mirror-maker
+  kafka-run-class
+  kafka-verifiable-consumer
+  kafka-configs
+  kafka-delegation-tokens
+  kafka-preferred-replica-election
+  kafka-server-start
+  kafka-verifiable-producer
+  kafka-console-consumer
+  kafka-delete-records
+  kafka-producer-perf-test
+  kafka-server-stop
+  kafka-console-producer
+  kafka-dump-log
+  kafka-reassign-partitions
+  kafka-streams-application-reset
+  # from cp-schema-registry
+  kafka-avro-console-consumer
+  kafka-avro-console-producer
+)
 
-function kpfhipg() {
-  _export_namespace "$1"
-  export PORT=${2:-5431}
-  kubectl port-forward -n "$NAMESPACE" svc/hafas-importer-postgres $PORT:5432
-}
-
-# kacc = kafka-avro-console-consumer
-function kacc() {
-  # shellcheck disable=SC2068
-  docker run --rm --net host \
-    dockerregistry.reisendeninfo.aws.db.de:5000/confluentinc.bwp/cp-schema-registry:5.3.3 \
-    kafka-avro-console-consumer \
-    --bootstrap-server "kafka.${NAMESPACE}.rikern-ingress-${CLUSTER}.reisendeninfo.aws.db.de:9092" \
-    --consumer-property security.protocol=SSL \
-    --property "schema.registry.url=https://schemaregistry.${NAMESPACE}.rikern-ingress-${CLUSTER}.reisendeninfo.aws.db.de:8081" \
-    $@
-}
-
-function kacc-ridi-dev-live() {
-  export NAMESPACE="ridi-dev-live"
-  export CLUSTER="k8s02-dev-ridienste"
-  # shellcheck disable=SC2068
-  kacc $@
-}
-
-function kacc-ridi-test-live() {
-  export NAMESPACE="ridi-test-live"
-  export CLUSTER="k8s02-dev-ridienste"
-  # shellcheck disable=SC2068
-  kacc $@
-}
-
-function kacc-riplf-dev-smoky03() {
-  export NAMESPACE="riplf-dev-smoky03"
-  export CLUSTER="k8s03-dev-riplattform"
-  # shellcheck disable=SC2068
-  kacc $@
-}
-
-function kacc-riplf-test-release03() {
-  export NAMESPACE="riplf-test-release03"
-  export CLUSTER="k8s03-test-riplattform"
-  # shellcheck disable=SC2068
-  kacc $@
-}
-
-function kacc-riplf-prd() {
-  export NAMESPACE="riplf-prd"
-  export CLUSTER="k8s03-prd-riplattform"
-  # shellcheck disable=SC2068
-  kacc $@
-}
+for cp_kafka_tool in "${cp_kafka_tools[@]}"; do
+  # shellcheck disable=SC2140
+  # shellcheck disable=SC2139
+  alias "$cp_kafka_tool"="_run_in_sherlock_container $cp_kafka_tool"
+done
